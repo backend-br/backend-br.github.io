00fc09dc98d073419ea22b8093b6e070a38356b1
Diego Costa <diego@diegocosta.com.br>
2017-09-01 10:42:16 -0300

    Reorganizado o Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './style.css';
+
+export default props => (
+    <nav className="header">
+        <div className="container">
+            <div className="header__logo">BackEnd Brasil</div>
+            <p className="header__description">A maior comunidade de BackEnd do Brasil</p>
+        </div>
+    </nav>
+);
