fd8b9a423d0c462678532fd61f9a90e5bc1f976c
Diego Costa <diego@diegocosta.com.br>
2017-09-01 10:41:39 -0300

    Reorganizado o Rodapé

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './style.css';
+
+export default props => (
+    <footer className="footer">	    
+        <div className="container">
+            <p className="footer__text"> MIT © BackEnd Brasil 2017. </p>
+        </div>
+    </footer>
+);
