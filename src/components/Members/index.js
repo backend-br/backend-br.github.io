aa6b65b677e16dfb918053bae27db6118b51c276
Diego Costa <diego@diegocosta.com.br>
2017-09-01 10:43:14 -0300

    Adicionado a listagem de membros

---
+++ src/components/Members/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import Member from '../Member';
+
+import { getMembers } from '../../services/github';
+
+import './style.css';
+
+class Members extends Component 
+{
+    componentWillMount()
+    {
+        this.state = { members: [] };
+
+        getMembers(this.props.org).then(response => this.setState({ members: response.data }));
+    }
+
+    renderMembers()
+    {
+        return this.state.members.map(member => <Member key={ member.id } {...member } />);
+    }
+
+    render()
+    {
+        return(
+            <div className="members">
+                <div className="container">
+                    <h2 className="members__title">Nosso time</h2>
+                    <ul className="members__list">
+                        { this.renderMembers() }
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Members;
