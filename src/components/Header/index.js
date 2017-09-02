72323885670ce2f537246c88169603aea9038dbc
Diego Costa <diego@diegocosta.com.br>
2017-09-02 08:10:29 -0300

    Adicionado botão do Slack

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 
 import './style.css';
 
+import SlackButton from '../SlackButton';
+
 export default props => (
     <nav className="header">
         <div className="container">
-            <div className="header__logo">BackEnd Brasil</div>
-            <p className="header__description">A maior comunidade de BackEnd do Brasil</p>
+            <div className="header__content">
+                <div className="header__branding">
+                    <div className="header__logo">BackEnd Brasil</div>
+                    <p className="header__description">A maior comunidade de BackEnd do Brasil</p>
+                </div>
+                <SlackButton />
+            </div>
         </div>
     </nav>
 );
