7fd9c718151509ffd63547d06008524ba0bcd3d5
Diego Costa <diego@diegocosta.com.br>
2017-09-03 01:13:45 -0300

    Realizados ajustes para uma melhor visualização em dispositivos moveis

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,12 +7,18 @@ import SlackButton from '../SlackButton';
 export default props => (
     <nav className="header">
         <div className="container">
-            <div className="header__content">
-                <div className="header__branding">
-                    <a href="https://github.com/backend-br" className="header__logo">BackEnd Brasil</a>
-                    <p className="header__description">A maior comunidade de BackEnd do Brasil</p>
+            <div className="row">
+                <div className="col-xs-12 col-sm-8">
+                    <div className="header__branding">
+                        <a href="https://github.com/backend-br" className="header__logo">BackEnd Brasil</a>
+                        <p className="header__description">A maior comunidade de BackEnd do Brasil</p>
+                    </div>
                 </div>
-                <SlackButton />
+                <div className="col-xs-12 col-sm-4">
+                    <div className="header__button">
+                        <SlackButton />
+                    </div>
+                </div>
             </div>
         </div>
     </nav>
