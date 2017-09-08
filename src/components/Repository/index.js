ab2faeb29c00323af6b874a8c93015458255853f
Diego Costa <diego@diegocosta.com.br>
2017-09-08 10:05:55 -0300

    Adicionado a quantidade de issues abertas nos repositórios

--- src/components/Repository/index.js
+++ src/components/Repository/index.js
@@ -4,6 +4,7 @@ import Emoji from 'react-emoji-render';
 import './style.css';
 
 import star from './icons/star.svg';
+import megaphone from './icons/megaphone.svg';
 
 export default props => (
     <div className="col-xs-12 col-sm-4">
@@ -13,8 +14,13 @@ export default props => (
                     <p className="repository__title">/{ props.name }</p>
                     <p><Emoji text={props.description} /></p>
                 </div>
-                <div className="repository__star">
-                    <img src={ star } className="repository__star__icon_star" alt="star" /> { props.stargazers_count }
+                <div className="repository__icons">
+                    <div className="repository__icons__icon">
+                        <img src={ star } className="repository__icons__icon__image" alt="star" /> { props.stargazers_count }
+                    </div>
+                    <div className="repository__icons__icon">
+                        <img src={ megaphone } className="repository__icons__icon__image" alt="issues" /> { props.open_issues_count }
+                    </div>
                 </div>
             </a>
         </div>
