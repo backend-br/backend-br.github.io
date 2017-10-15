683b08af62084fca2aa1deb13007490ca7e6f04a
Diego Costa <diego@diegocosta.com.br>
2017-10-15 14:33:24 -0300

    Removido a descrição do header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -15,7 +15,6 @@ export default () => (
         <div className="col-xs-12 col-sm-8">
           <div className="header__branding">
             <a href="https://github.com/backend-br" className="header__logo">BackEnd Brasil</a>
-            <p className="header__description">A maior comunidade de BackEnd do Brasil</p>
           </div>
         </div>
         <div className="col-xs-12 col-sm-4">
