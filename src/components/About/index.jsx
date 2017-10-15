db82f3167572311bb9d7bfd8c5ea2afbf9c2937a
Diego Costa <diego@diegocosta.com.br>
2017-10-15 09:41:28 -0300

    Criado componente About

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -11,17 +11,16 @@ const slackInviteUrl = 'https://backend-br.slack.com/join/shared_invite/MjI2MTM2
 const githubProfileUrl = 'https://github.com/backend-br';
 
 export default () => (
-  <div className="container-fluid">
+  <div className="container">
     <div className="row">
       <div className="about">
-        <h1>Quem somos?</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia labore in optio iste eius ut. Incidunt, tenetur. Perferendis ipsa, aliquam totam exercitationem commodi aperiam, quam nulla cum alias amet voluptates, ipsum deserunt quis vitae quos officiis qui! Nam reiciendis iste, nihil eveniet id omnis laudantium eligendi ipsa aut corporis possimus!</p>
-
+        <p>A comunidade Back-End BR tem como objetivo unir desenvolvedore(a)s Back-End, <br /> que tenham qualquer conhecimento, para discutir, trazer suas dúvidas e trocar conhecimento entre si.</p>
+    
         <div className="about__buttons">
           <Button icon={githubIcon} iconAlt="Logo do Github" link={githubProfileUrl} label="Github" />
           <Button icon={slackIcon} iconAlt="Logo do Slack" link={slackInviteUrl} label="Slack" />
         </div>
       </div>
-      </div>
+    </div>
   </div>
 );
