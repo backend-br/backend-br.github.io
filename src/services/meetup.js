57663e9f8691a3e56f0dcb0a826e2baee7e20fce
Gustavo Castilião <gucastiliao@gmail.com>
2018-03-13 21:27:43 -0300

    Corrigindo link para comunicação com a API do Meetup

--- src/services/meetup.js
+++ src/services/meetup.js
@@ -3,7 +3,7 @@
 import Axios from 'axios';
 
 const getMeetup = async (group, endpoint) => {
-  const url = `https://meetup-api-proxy.herokuapp.com/${group}/${endpoint}?photo-host=public&page=20&status=upcoming,past`;
+  const url = `https://zuevcrixk8.execute-api.us-east-1.amazonaws.com/prod/call?group=${group}&endpoint=${endpoint}&photo-host=public&page=20&status=upcoming,past`;
   const response = await Axios.get(url);
 
   return response.data;
