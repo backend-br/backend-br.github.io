aa16e378c7ea9a6a3fe801936b3e9e4cf7745f82
Matheus Hernandes <midia.matheus@gmail.com>
2018-10-02 16:13:54 -0300

    arrumei as tarefas do gulp pra limpar, concatenar, transpilar e produzir o js corretamente configurei pro CSS ser comprimido

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,66 @@
 const gulp = require('gulp')
-const concat = require('gulp-concat')
-const uglify = require('gulp-uglify')
-const { exec } = require('child_process')
 const browserSync = require('browser-sync')
+const concat = require('gulp-concat')
+const plumber = require('gulp-plumber')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+const fs = require('fs')
+const path = require('path')
+const unlink = f => new Promise((res, rej) => fs.unlink(f, e => e ? rej(e) : res()))
+const readDir = f => new Promise((res, rej) => fs.readdir(f, (e, files) => e ? rej(e) : res(files)))
+const { exec } = require('child_process')
 
-gulp.task('hexo', function () {
+/**
+ * Use hexo to generate files
+ * We had some problems with the hexo's API so now
+ * we're going with child_process's spawn
+ */
+gulp.task('hexo', function (cb) {
   const command = 'hexo generate'
   exec(command, (err, stdout, stderr) => {
     if (err) {
-      console.error(err)
+      return cb(err)
     }
+
+    browserSync.reload()
+    cb()
   })
 })
 
-gulp.task('js', function () {
-  gulp.src('./public/*.js')
-    .pipe(uglify())
+/**
+ * Clear JS files from ./public
+ */
+gulp.task('clear-js-files', function () {
+  return readDir('./public')
+    .then(files => {
+      files = files
+        .filter(f => f.match(/\.js/g))
+        .map(f => path.resolve(process.cwd(), 'public', f))
+
+      return Promise.all(files.map(unlink))
+    })
+})
+
+/**
+ * Process JS files
+ * Match files from the theme folder
+ * concat them, transpile to ES5
+ * and uglify them
+ */
+gulp.task('process-js', ['clear-js-files'], function () {
+  return gulp.src('./themes/backendbrasil/source/*.js')
+    .pipe(plumber())
     .pipe(concat('main.min.js'))
-    .pipe(gulp.dest('./public'))
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('public'))
+})
+
+gulp.task('js', ['process-js'], function (cb) {
+  browserSync.reload()
+  cb()
 })
 
 gulp.task('server', function () {
@@ -27,13 +70,21 @@ gulp.task('server', function () {
     }
   })
 
-  gulp.watch('./public/*', browserSync.reload)
-  gulp.watch('./public/*.js', ['js'])
+  // Handlebars Files
   gulp.watch('./themes/backendbrasil/layout/*.hbs', ['hexo'])
-  gulp.watch('./themes/backendbrasil/layout/partials/*.hbs', ['hexo'])
-  gulp.watch('./themes/backendbrasil/source/*.{scss,js}', ['hexo'])
+  gulp.watch('./themes/backendbrasil/layout/**/*.hbs', ['hexo'])
+
+  // SCSS Files
+  gulp.watch('./themes/backendbrasil/source/*.scss', ['hexo'])
   gulp.watch('./themes/backendbrasil/source/**/*.scss', ['hexo'])
+
+  // Image Files
   gulp.watch('./themes/backendbrasil/source/*.{jpg,gif,png}', ['hexo'])
+
+  // JS Files
+  gulp.watch('./themes/backendbrasil/source/*.js', ['js'])
+  gulp.watch('./themes/backendbrasil/source/**/*.js', ['js'])
 })
 
 gulp.task('default', ['hexo', 'js', 'server'])
+//gulp.task('default', ['js'])
