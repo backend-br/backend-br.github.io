95360fa59bbb8119156ec593fa3cc8e3c1784c42
Matheus Hernandes <midia.matheus@gmail.com>
2018-10-02 07:34:39 -0300

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
-const Hexo = require('hexo')
-const hexo = new Hexo(process.cwd(), {})
 const gulp = require('gulp')
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+const { exec } = require('child_process')
 const browserSync = require('browser-sync')
 
 gulp.task('hexo', function () {
-  return hexo.init()
-    .then(() => hexo.call('generate', { watch: true }))
-    .then(() => hexo.exit())
+  const command = 'hexo generate'
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err)
+    }
+  })
+})
+
+gulp.task('js', function () {
+  gulp.src('./public/*.js')
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest('./public'))
 })
 
 gulp.task('server', function () {
@@ -17,6 +28,12 @@ gulp.task('server', function () {
   })
 
   gulp.watch('./public/*', browserSync.reload)
+  gulp.watch('./public/*.js', ['js'])
+  gulp.watch('./themes/backendbrasil/layout/*.hbs', ['hexo'])
+  gulp.watch('./themes/backendbrasil/layout/partials/*.hbs', ['hexo'])
+  gulp.watch('./themes/backendbrasil/source/*.{scss,js}', ['hexo'])
+  gulp.watch('./themes/backendbrasil/source/**/*.scss', ['hexo'])
+  gulp.watch('./themes/backendbrasil/source/*.{jpg,gif,png}', ['hexo'])
 })
 
-gulp.task('default', ['hexo', 'server'])
+gulp.task('default', ['hexo', 'js', 'server'])
