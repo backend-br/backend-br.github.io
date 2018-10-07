b0ae70f92bc618bfdcf1e6ef39e4d8f418e18898
Matheus Hernandes <midia.matheus@gmail.com>
2018-10-07 18:54:27 -0300

    add events section

--- themes/backendbrasil/helper/index.js
+++ themes/backendbrasil/helper/index.js
@@ -3,10 +3,16 @@ const i18n = new I18n({languages: ['pt']})
 const pt = require('../languages/pt.json')
 const helpers = require('handlebars-helpers')()
 i18n.set('pt', pt)
-const stringify = data => Array.isArray(data) ? data.join('.') : data
+const stringify = data => {
+  if (Array.isArray(data)) {
+    return data.filter(f => typeof f === 'string').join('.')
+  }
+
+  return data
+}
 
 module.exports = hexo => ({
-  __: key => i18n.__()(stringify(key)),
-  _p: key => i18n._p()(stringify(key)),
+  __: (...key) => i18n.__()(stringify(key)),
+  _p: (...key) => i18n._p()(stringify(key)),
   ...helpers
 })
